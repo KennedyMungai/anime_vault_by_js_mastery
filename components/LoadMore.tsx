2725cfcbb274fa62ed021204114509cbbe934a81
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-23 11:55:25 +0300

    Made use of useInView and made the component a client component

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,21 +1,25 @@
-import Image from "next/image";
+'use client'
+import Image from 'next/image'
+import { useInView } from 'react-intersection-observer'
 
 function LoadMore() {
-  return (
-    <>
-      <section className="flex justify-center items-center w-full">
-        <div>
-          <Image
-            src="./spinner.svg"
-            alt="spinner"
-            width={56}
-            height={56}
-            className="object-contain"
-          />
-        </div>
-      </section>
-    </>
-  );
+	const { ref, inView } = useInView()
+
+	return (
+		<>
+			<section className='flex justify-center items-center w-full'>
+				<div>
+					<Image
+						src='./spinner.svg'
+						alt='spinner'
+						width={56}
+						height={56}
+						className='object-contain'
+					/>
+				</div>
+			</section>
+		</>
+	)
 }
 
-export default LoadMore;
+export default LoadMore
