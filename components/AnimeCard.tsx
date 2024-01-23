7840e732a69846d5ea8d4ffabf7e0dbcda7f7b31
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-23 12:23:46 +0300

    Made the component a client component and applied some of the framer motion js logic

--- components/AnimeCard.tsx
+++ components/AnimeCard.tsx
@@ -1,71 +1,85 @@
-import Image from "next/image";
+'use client'
+import { motion as m } from 'framer-motion'
+import Image from 'next/image'
 
 export interface AnimeProp {
-  id: string;
-  name: string;
-  image: {
-    original: string;
-  };
-  kind: string;
-  episodes: number;
-  episodes_aired: number;
-  score: string;
+	id: string
+	name: string
+	image: {
+		original: string
+	}
+	kind: string
+	episodes: number
+	episodes_aired: number
+	score: string
 }
 
 interface Prop {
-  anime: AnimeProp;
-  index: number;
+	anime: AnimeProp
+	index: number
 }
 
 function AnimeCard({ anime }: Prop) {
-  return (
-    <div className="max-w-sm rounded relative w-full">
-      <div className="relative w-full h-[37vh]">
-        <Image
-          src={`https://shikimori.one/${anime.image.original}`}
-          alt={anime.name}
-          fill
-          className="rounded-xl"
-        />
-      </div>
-      <div className="py-4 flex flex-col gap-3">
-        <div className="flex justify-between items-center gap-1">
-          <h2 className="font-bold text-white text-xl line-clamp-1 w-full">
-            {anime.name}
-          </h2>
-          <div className="py-1 px-2 bg-[#161921] rounded-sm">
-            <p className="text-white text-sm font-bold capitalize">
-              {anime.kind}
-            </p>
-          </div>
-        </div>
-        <div className="flex gap-4 items-center">
-          <div className="flex flex-row gap-2 items-center">
-            <Image
-              src="./episodes.svg"
-              alt="episodes"
-              width={20}
-              height={20}
-              className="object-contain"
-            />
-            <p className="text-base text-white font-bold">
-              {anime.episodes || anime.episodes_aired}
-            </p>
-          </div>
-          <div className="flex flex-row gap-2 items-center">
-            <Image
-              src="./star.svg"
-              alt="star"
-              width={18}
-              height={18}
-              className="object-contain"
-            />
-            <p className="text-base font-bold text-[#FFAD49]">{anime.score}</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<m.div
+			className='max-w-sm rounded relative w-full'
+			initial={{ scale: 0, rotate: -90, opacity: 0 }}
+			animate={{ scale: 1, rotate: 0, opacity: 1 }}
+			transition={{
+				type: 'spring',
+				stiffness: 250,
+				damping: 20,
+				delay: 0.3
+			}}
+		>
+			<div className='relative w-full h-[37vh]'>
+				<Image
+					src={`https://shikimori.one/${anime.image.original}`}
+					alt={anime.name}
+					fill
+					className='rounded-xl'
+				/>
+			</div>
+			<div className='py-4 flex flex-col gap-3'>
+				<div className='flex justify-between items-center gap-1'>
+					<h2 className='font-bold text-white text-xl line-clamp-1 w-full'>
+						{anime.name}
+					</h2>
+					<div className='py-1 px-2 bg-[#161921] rounded-sm'>
+						<p className='text-white text-sm font-bold capitalize'>
+							{anime.kind}
+						</p>
+					</div>
+				</div>
+				<div className='flex gap-4 items-center'>
+					<div className='flex flex-row gap-2 items-center'>
+						<Image
+							src='./episodes.svg'
+							alt='episodes'
+							width={20}
+							height={20}
+							className='object-contain'
+						/>
+						<p className='text-base text-white font-bold'>
+							{anime.episodes || anime.episodes_aired}
+						</p>
+					</div>
+					<div className='flex flex-row gap-2 items-center'>
+						<Image
+							src='./star.svg'
+							alt='star'
+							width={18}
+							height={18}
+							className='object-contain'
+						/>
+						<p className='text-base font-bold text-[#FFAD49]'>
+							{anime.score}
+						</p>
+					</div>
+				</div>
+			</div>
+		</m.div>
+	)
 }
 
-export default AnimeCard;
+export default AnimeCard
