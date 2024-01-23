29bcbc6e48c906068db74d0aef097266adf924c4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-23 12:09:44 +0300

    Added some more anime to be rendered on the front page

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { fetchAnime } from '@/actions/action'
+import AnimeCard from '@/components/AnimeCard'
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
 import { useInView } from 'react-intersection-observer'
@@ -19,6 +20,11 @@ function LoadMore() {
 
 	return (
 		<>
+			<section className='grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10'>
+				{data.map((item: AnimeProp, index: number) => (
+					<AnimeCard key={item.id} anime={item} index={index} />
+				))}
+			</section>
 			<section className='flex justify-center items-center w-full'>
 				<div ref={ref}>
 					<Image
