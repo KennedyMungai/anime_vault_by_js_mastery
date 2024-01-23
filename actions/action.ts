6b994e4c59cba8f9d55efa372236f3c7be391b69
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-23 11:40:02 +0300

    Created the fetchAnime arrow function

---
+++ actions/action.ts
@@ -0,0 +1,8 @@
+'use server'
+
+export const fetchAnime = async () => {
+    const res = await fetch('https://shikimori.one/api/animes')
+    const data = await res.json()
+    console.log(data)
+    return data
+}
